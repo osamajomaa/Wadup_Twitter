1e84db1e94bb004d0ffac61dfb5674719b2e6f2b
osamajomaa <jomaao@miamioh.edu>
2014-12-06 23:53:18 -0500

    Added charting class

---
+++ src/DataVis.java
@@ -0,0 +1,148 @@
+import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import org.jfree.chart.ChartColor;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+
+
+public class DataVis {
+
+	public void plotBarChart(TreeMap<String,Integer> data, String title, String xTitle, String yTitle) {
+		DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+		for(Entry<String,Integer> entry : data.entrySet()) {
+			dataSet.addValue(entry.getValue(), "series", entry.getKey());
+		}
+		JFreeChart chart = ChartFactory.createBarChart(
+	            title,
+	            xTitle,
+	            yTitle,
+	            dataSet,
+	            PlotOrientation.VERTICAL,
+	            false,
+	            false,
+	            false
+	        );
+	 
+	        CategoryPlot plot = (CategoryPlot)chart.getPlot();
+	        CategoryAxis xAxis = (CategoryAxis)plot.getDomainAxis();
+	        xAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_45);
+	        
+	        chart.setBackgroundPaint(ChartColor.WHITE);
+	        try {
+	            ChartUtilities.saveChartAsPNG(new File("chart.png"), chart, 800, 600);
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        }
+	}
+	
+	public void plotLineChart(TreeMap<Integer,Integer> data, String title, String xTitle, String yTitle) {
+		DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+		for(Entry<Integer,Integer> entry : data.entrySet()) {
+			dataSet.addValue(entry.getValue(), "series", entry.getKey());
+		}
+		
+		JFreeChart chart = ChartFactory.createLineChart(
+	            title,       // chart title
+	            xTitle,                    // domain axis label
+	            yTitle,                   // range axis label
+	            dataSet,                   // data
+	            PlotOrientation.VERTICAL,  // orientation
+	            false,                      // include legend
+	            false,                      // tooltips
+	            false                      // urls
+	        );
+		chart.setBackgroundPaint(ChartColor.white);
+		
+        final CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        plot.setBackgroundPaint(ChartColor.lightGray);
+        plot.setRangeGridlinePaint(ChartColor.white);
+        
+        // customise the range axis...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setAutoRangeIncludesZero(true);
+        try {
+            ChartUtilities.saveChartAsPNG(new File("lineChart.png"), chart, 800, 600);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+	}
+	
+	public void plotMultipleLineCharts(List<TreeMap<Integer,Integer>> data, String title, String xTitle, String yTitle) {
+		DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+		for(int i=0; i<data.size(); i++) {
+			for(Entry<Integer,Integer> entry : data.get(i).entrySet()) {
+				dataSet.addValue(entry.getValue(), "series"+i, entry.getKey());
+			}
+		}
+		
+		JFreeChart chart = ChartFactory.createLineChart(
+	            title,       // chart title
+	            xTitle,                    // domain axis label
+	            yTitle,                   // range axis label
+	            dataSet,                   // data
+	            PlotOrientation.VERTICAL,  // orientation
+	            false,                      // include legend
+	            false,                      // tooltips
+	            false                      // urls
+	        );
+		chart.setBackgroundPaint(ChartColor.white);
+		
+        final CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        plot.setBackgroundPaint(ChartColor.lightGray);
+        plot.setRangeGridlinePaint(ChartColor.white);
+        
+        // customise the range axis...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setAutoRangeIncludesZero(true);
+        try {
+            ChartUtilities.saveChartAsPNG(new File("lineChart.png"), chart, 800, 600);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+	}
+	
+	public static void plotPieChart(TreeMap<String,Integer> data, String title) {
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		for(Entry<String,Integer> entry : data.entrySet()) {
+			dataset.setValue(entry.getKey(), entry.getValue());
+		}
+		JFreeChart chart = ChartFactory.createPieChart(
+	            title,  // chart title
+	            dataset,             // data
+	            true,               // include legend
+	            true,
+	            false
+	        );
+
+	        PiePlot plot = (PiePlot) chart.getPlot();
+	        plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
+	        plot.setNoDataMessage("No data available");
+	        plot.setCircular(false);
+	        plot.setLabelGap(0.02);
+	        
+	        try {
+	            ChartUtilities.saveChartAsPNG(new File("pieChart.png"), chart, 800, 600);
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        }
+	}
+
+}
